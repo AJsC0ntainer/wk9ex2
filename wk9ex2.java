f2913f3151dbc14a91c08e9d09f8fe7c358ea565
AJ Cunha <argenisjc@hotmail.com>
2025-03-23 15:45:05 -0500

    Comments Added

--- wk9ex2.java
+++ wk9ex2.java
@@ -1,30 +1,40 @@
-import java.util.Scanner;  // Import the Scanner class
+ //Import the Scanner class
+import java.util.Scanner; 
 
 public class wk9ex2
 {
     public static void main(String[] args)
     {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-
+        //Create a Scanner object.
+        Scanner myObj = new Scanner(System.in); 
+        //Program Title.
         System.out.println("AREA CALCULATOR APP");
-
+        //Ask the user to enter the length of the rectangle.
         System.out.print("Enter the length of the rectangle: ");
-        String input = myObj.nextLine();  // Read user input
+        //Declare a variable to store the user input and store the user input in the variable.
+        String input = myObj.nextLine(); 
+        //Convert the user input to a double.
         double length = Double.parseDouble(input);
-
+        //Ask the user to enter the width of the rectangle.
         System.out.print("Enter the width of the rectangle: ");
-        input = myObj.nextLine();  // Read user input
+        //Declare a variable to store the user input and store the user input in the variable.
+        input = myObj.nextLine();
+        //Convert the user input to a double.
         double width = Double.parseDouble(input);
-
+        //Formula to calculate the area of the rectangle.
         double area = length * width;
+        //Formula to calculate the perimeter of the rectangle.
         double perimeter = 2 * (length + width);
-
+        //Display the results.
         System.out.println("Results:");
         System.out.println("Area of the rectangle: " + String.format("%.0f", area));
         System.out.println("Perimeter of the rectangle: " + String.format("%.0f", perimeter));
 
+        //Close the Scanner object.
         myObj.close();
-
+        //End of program message.
         System.out.println("\nEnd of program!\n");
+
+        //Pushed to GitHub Wk9ex2 Repository.
     }
 }
